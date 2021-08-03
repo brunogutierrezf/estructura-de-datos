78506778832e30538096cf841e8c3fdbc823f3aa
brunogutierrezf <88397427+brunogutierrezf@users.noreply.github.com>
2021-08-03 13:17:29 -0400

    Add files via upload

---
+++ Area del triangulo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{ 
+float A=0,h,BGF;
+    cout<<"Ingrese la base: "<<endl;
+    cin>>BGF;cout<<endl;
+    cout<<"Ingrese la altura: "<<endl;
+    cin>>h;cout<<endl;
+    A=(BGF*h)/2;
+    cout<<"El area del triangulo es: "<<A<<endl;
+    
+    
+    return 0;
+}
